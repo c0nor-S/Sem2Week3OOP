a1411fbc98689415e8e4bdd5b2b5a840a35ed2d7
c0nor-S <150271462+c0nor-S@users.noreply.github.com>
2026-02-15 16:59:17 +0000

    Second Commit - @Service Added For Functionality

--- src/main/java/ie/atu/sem2/week3/service/CalculatorService.java
+++ src/main/java/ie/atu/sem2/week3/service/CalculatorService.java
@@ -1,5 +1,8 @@
 package ie.atu.sem2.week3.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
     public int add(int a, int b) {
         return a + b;
