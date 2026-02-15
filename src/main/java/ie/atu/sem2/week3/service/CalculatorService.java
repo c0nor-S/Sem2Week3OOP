bd7703ee146866c9f8d870d23a99b5fde09d2424
c0nor-S <150271462+c0nor-S@users.noreply.github.com>
2026-02-15 16:57:06 +0000

    First Commit - Added Package Called Service And Class Called CalculatorService. Addition And Subtraction Functions Implemented

---
+++ src/main/java/ie/atu/sem2/week3/service/CalculatorService.java
@@ -0,0 +1,10 @@
+package ie.atu.sem2.week3.service;
+
+public class CalculatorService {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
